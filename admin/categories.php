495c5a4d44ac7e9ed267c7d8ede1fe6eaf2094b8
Pavle <paja98100@gmail.com>
2021-04-19 21:40:02 +0200

    adding functionality to edit buttons and edit field

--- admin/categories.php
+++ admin/categories.php
@@ -41,38 +41,14 @@
                             </div>
                         </form>
 
-                        <form action="" method="post">
-                            <div class="form-group">
-                                <label for="cat-title">Edit Category</label>
+                        <?php
 
-                                <?php
-                                if (isset($_GET['edit'])) {
-                                    $cat_id = $_GET['edit'];
+                        if (isset($_GET['edit'])) {
+                            $cat_id = $_GET['edit'];
+                            include "includes/update_categories.php";
+                        }
 
-                                    $query = "SELECT * FROM categories WHERE cat_id = $cat_id";
-                                    $selected_categories_id = mysqli_query($connection, $query);
-
-                                    while ($row = mysqli_fetch_assoc($selected_categories_id)) {
-                                        $cat_id = $row['cat_id'];
-                                        $cat_title = $row['cat_title'];
-
-                                ?>
-
-                                        <input value="<?php if (isset($cat_title)) {
-                                                            echo $cat_title;
-                                                        } ?>" class="form-control" type="text" name="cat_title">
-
-
-                                <?php
-                                    }
-                                }
-                                ?>
-
-                            </div>
-                            <div class="form-group">
-                                <input class="btn btn-primary" type="submit" name="submit" value="Update Category">
-                            </div>
-                        </form>
+                        ?>
 
                     </div>
                     <!-- add category form -->
