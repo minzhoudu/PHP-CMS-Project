015fd5ea135b388d2faa0c84b85e17b62cc7a26f
Pavle <paja98100@gmail.com>
2021-04-19 21:24:13 +0200

    adding edit buttons for categories

--- admin/categories.php
+++ admin/categories.php
@@ -40,6 +40,40 @@
                                 <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
                         </form>
+
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="cat-title">Edit Category</label>
+
+                                <?php
+                                if (isset($_GET['edit'])) {
+                                    $cat_id = $_GET['edit'];
+
+                                    $query = "SELECT * FROM categories WHERE cat_id = $cat_id";
+                                    $selected_categories_id = mysqli_query($connection, $query);
+
+                                    while ($row = mysqli_fetch_assoc($selected_categories_id)) {
+                                        $cat_id = $row['cat_id'];
+                                        $cat_title = $row['cat_title'];
+
+                                ?>
+
+                                        <input value="<?php if (isset($cat_title)) {
+                                                            echo $cat_title;
+                                                        } ?>" class="form-control" type="text" name="cat_title">
+
+
+                                <?php
+                                    }
+                                }
+                                ?>
+
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" name="submit" value="Update Category">
+                            </div>
+                        </form>
+
                     </div>
                     <!-- add category form -->
                     <div class="col-xs-6">
@@ -62,6 +96,7 @@
                                     echo "<tr>";
                                     echo "<td>{$cat_title}</td>";
                                     echo "<td> <a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+                                    echo "<td> <a href='categories.php?edit={$cat_id}'>Edit</a></td>";
                                     echo "</tr>";
                                 }
                                 ?>
