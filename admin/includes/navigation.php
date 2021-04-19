c1fabf6b1f7a8d8313544beeed4ab58d08928277
Pavle <paja98100@gmail.com>
2021-04-19 16:56:20 +0200

    admin sidebar organised

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -34,13 +34,26 @@
                 <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
             </li>
             <li>
-                <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> Bootstrap Elements</a>
+                <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
+                <ul id="posts_dropdown" class="collapse">
+                    <li>
+                        <a href="#">View All Posts</a>
+                    </li>
+                    <li>
+                        <a href="#">Add Posts</a>
+                    </li>
+                </ul>
             </li>
             <li>
-                <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Bootstrap Grid</a>
+                <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
             </li>
+
+            <li class="active">
+                <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments</a>
+            </li>
+
             <li>
-                <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
+                <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="demo" class="collapse">
                     <li>
                         <a href="#">Dropdown Item</a>
@@ -50,11 +63,8 @@
                     </li>
                 </ul>
             </li>
-            <li class="active">
-                <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Blank Page</a>
-            </li>
             <li>
-                <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> RTL Dashboard</a>
+                <a href="bootstrap-grid.html"><i class="fa fa-fw fa fa-user"></i> Profile</a>
             </li>
         </ul>
     </div>
